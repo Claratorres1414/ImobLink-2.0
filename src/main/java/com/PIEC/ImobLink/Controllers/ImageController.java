5e85d1cdb22ba90912f5cb9f7bb55e7d0f0eac8f
claratorres1414 <claratorres12345679@gmail.com>
2025-10-02 10:06:32 -0300

    #18 - Alteração no ImageService no método saveImage para manter o padrão ao nomear os arquivos; Adicionado novo método no ImageService, o getImageByPostId que retorna a imagem endereçada naquele post em caso de suceso, em caso de erro propaga uma exceção de erro ao buscar imagem ou um erro de usuário não encontrado em caso do usuário não estar sendo autenticado; Adicionado novo endpoint de Get no ImageController para fazer a requisição da imagem no ImageService, recebendo o id do Post como parâmetro e a autenticação do usuário.

--- src/main/java/com/PIEC/ImobLink/Controllers/ImageController.java
+++ src/main/java/com/PIEC/ImobLink/Controllers/ImageController.java
@@ -5,10 +5,7 @@ import io.jsonwebtoken.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -16,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class ImageController {
     private final ImageService imageService;
+
     @PostMapping
     public ResponseEntity<String> saveImage(@RequestParam("image") MultipartFile image, Authentication auth) throws IOException {
         try{
@@ -23,6 +21,15 @@ public class ImageController {
             return ResponseEntity.ok("Imagem salvo com sucesso: " + imageResponse);
         } catch (Exception e){
             throw new IOException("Erro ao salvar a imagem ou ao tentar autenticar o usuário: " + e.getMessage() + " tente novamente");
+        }
+    }
+
+    @GetMapping("/{id}/post")
+    public ResponseEntity<byte[]> getImage(@PathVariable Long id, Authentication auth) throws IOException {
+        try{
+            return imageService.getImageByPostId(id, auth);
+        } catch (Exception e){
+            throw new IOException("Erro ao buscar imagem: " + e.getMessage());
         }
     }
 }
--- src/main/java/com/PIEC/ImobLink/Services/ImageService.java
+++ src/main/java/com/PIEC/ImobLink/Services/ImageService.java
@@ -1,25 +1,34 @@
 package com.PIEC.ImobLink.Services;
 
 import com.PIEC.ImobLink.Entitys.Images;
+import com.PIEC.ImobLink.Entitys.Post;
 import com.PIEC.ImobLink.Entitys.User;
 import com.PIEC.ImobLink.Repositorys.ImageRepository;
 import com.PIEC.ImobLink.Repositorys.UserRepository;
 import io.jsonwebtoken.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
     private final UserRepository userRepository;
+    private final PostService postService;
 
     public String saveImage(MultipartFile file, Authentication auth) throws IOException, java.io.IOException {
         String email = auth.getName();
@@ -32,7 +41,7 @@ public class ImageService {
         Files.write(Paths.get(filePath), file.getBytes());
 
         Images image = new Images();
-        image.setFilename(file.getOriginalFilename());
+        image.setFilename(UUID.randomUUID() + "_" + file.getOriginalFilename());
         image.setFilepath(filePath);
         image.setContentType(file.getContentType());
         image.setUser(user);
@@ -43,5 +52,28 @@ public class ImageService {
         }
 
         return image.getFilepath();
+    }
+
+    public ResponseEntity<byte[]> getImageByPostId(@PathVariable Long postId, Authentication auth) throws java.io.IOException {
+        userRepository.findByEmail(auth.getName()).orElseThrow(() -> new UsernameNotFoundException(auth.getName()));
+        try{
+            Post post = postService.get(postId);
+            String imagePath = post.getImagePath();
+
+            if (imagePath == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Imagem não encontrada para o post");
+            }
+
+            File file = new File(imagePath);
+            byte[] imageBytes = Files.readAllBytes(file.toPath());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+            headers.setContentLength(imageBytes.length);
+
+            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            throw new IOException("Erro ao buscar imagem: " + e.getMessage());
+        }
     }
 }
