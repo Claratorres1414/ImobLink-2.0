974aa91fc72e615cf3d3e38de87683cff3b17b8f
Cauatop <pedrocaua322@gmail.com>
2025-10-08 08:40:43 -0300

    #29 - Bug corrigido

--- src/main/java/com/PIEC/ImobLink/Configurations/MultipartInputStreamFileResource.java
+++ src/main/java/com/PIEC/ImobLink/Configurations/MultipartInputStreamFileResource.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.core.io.InputStreamResource;
 
+import io.jsonwebtoken.io.IOException;
+
 import java.io.InputStream;
 
 @Getter
@@ -15,5 +17,9 @@ public class MultipartInputStreamFileResource extends InputStreamResource {
     public MultipartInputStreamFileResource(InputStream inputStream, String filename) {
         super(inputStream);
         this.filename = filename;
+    }
+    @Override
+    public long contentLength() throws IOException {
+        return -1; // we do not want to generally read the whole stream into memory ...
     }
 }
