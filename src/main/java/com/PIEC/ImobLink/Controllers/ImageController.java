abd43f809f0e90d61b6a5dab44280016928cc030
claratorres1414 <claratorres12345679@gmail.com>
2025-10-15 08:24:46 -0300

    #38 - Atualização no ImageController para que o método saveImage tente requisitar o serviço de salvar imagem, em caso positivo retorna uma mensagem e o caminho da imagem e em caso negativo pode retornar um BadRequest ou em caso de "quebra" propaga uma excessão; Modificação de GetMapping para que haja um getMapping que carrega a thumb da publicação, um get para retornar os dados de todas as imagens da publicação e um get para carregar uma imagem pelo id.

--- src/main/java/com/PIEC/ImobLink/Controllers/ImageController.java
+++ src/main/java/com/PIEC/ImobLink/Controllers/ImageController.java
@@ -1,5 +1,7 @@
 package com.PIEC.ImobLink.Controllers;
 
+import com.PIEC.ImobLink.DTOs.ImageResponse;
+import com.PIEC.ImobLink.Entitys.Images;
 import com.PIEC.ImobLink.Services.ImageService;
 import io.jsonwebtoken.io.IOException;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/images")
@@ -17,19 +21,32 @@ public class ImageController {
     @PostMapping
     public ResponseEntity<String> saveImage(@RequestParam("image") MultipartFile image, Authentication auth) throws IOException {
         try{
-            String imageResponse = imageService.saveImage(image, auth);
-            return ResponseEntity.ok("Imagem salvo com sucesso: " + imageResponse);
+            Images imageResponse = imageService.saveImage(image, auth);
+            if(imageResponse != null) {
+                return ResponseEntity.ok("Imagem salvo com sucesso com caminho: " + imageResponse.getFilepath());
+            }
+            return ResponseEntity.badRequest().body("Erro ao salvar imagem");
         } catch (Exception e){
             throw new IOException("Erro ao salvar a imagem ou ao tentar autenticar o usuário: " + e.getMessage() + " tente novamente");
         }
     }
 
-    @GetMapping("/{id}/post")
-    public ResponseEntity<byte[]> getImage(@PathVariable Long id, Authentication auth) throws IOException {
+    @GetMapping("/{postId}/post/thumb")
+    public ResponseEntity<byte[]> getFirstImage(@PathVariable Long postId, Authentication auth) throws IOException {
         try{
-            return imageService.getImageByPostId(id, auth);
+            return imageService.getFirstImageByPostId(postId, auth);
         } catch (Exception e){
             throw new IOException("Erro ao buscar imagem: " + e.getMessage());
         }
+    }
+
+    @GetMapping("/{postId}/post/all")
+    public ResponseEntity<List<ImageResponse>> getAllImagesByPostId(@PathVariable Long postId, Authentication auth) throws IOException {
+        return ResponseEntity.ok(imageService.getAllImagesByPostId(postId, auth));
+    }
+
+    @GetMapping("/get/{imageId}")
+    public ResponseEntity<byte[]> getImageById(@PathVariable Long imageId, Authentication auth) throws IOException, java.io.IOException {
+        return imageService.getImageById(imageId, auth);
     }
 }
