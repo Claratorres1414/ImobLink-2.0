42f522602a4eb42265171fbf42f64cfc53f24111
claratorres1414 <claratorres12345679@gmail.com>
2025-10-19 12:03:40 -0300

    #41 - Algumas correções na entidade Comment para garantir que o servidor suba corretamente, havia esquecido das anotações de Data e Entity, também havia esquecido de colocar o createAt como parte da contrução do comentário

--- src/main/java/com/PIEC/ImobLink/Entitys/Comment.java
+++ src/main/java/com/PIEC/ImobLink/Entitys/Comment.java
@@ -2,17 +2,26 @@ package com.PIEC.ImobLink.Entitys;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "comments")
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
