6ceac37818e4a17881c460eddbc784092d710e9e
claratorres1414 <claratorres12345679@gmail.com>
2025-10-19 11:57:16 -0300

    #41 - Criado o repositório de comentários com um método para listar os comentários por post em ordem de publicação e criada a entidade comentário que tem uma tabela própria chamada comments onde cada comentário tem um id, conteúdo, usuário e post

---
+++ src/main/java/com/PIEC/ImobLink/Entitys/Comment.java
@@ -0,0 +1,24 @@
+package com.PIEC.ImobLink.Entitys;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
---
+++ src/main/java/com/PIEC/ImobLink/Repositorys/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.PIEC.ImobLink.Repositorys;
+
+import com.PIEC.ImobLink.Entitys.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPostIdOrderByCreatedAtDesc(Long postId);
+}
