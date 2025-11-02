82ab4e1f8cfc189a269f2640bbbc139d8ad7012a
claratorres1414 <claratorres12345679@gmail.com>
2025-11-02 08:32:20 -0300

    #69 - atualização no FollowRespository para que tenha um método de busca mais eficiente e melhorado que sirva para o funcionamento adequado do FollowService

--- src/main/java/com/PIEC/ImobLink/Repositorys/FollowRespository.java
+++ src/main/java/com/PIEC/ImobLink/Repositorys/FollowRespository.java
@@ -3,9 +3,8 @@ package com.PIEC.ImobLink.Repositorys;
 import com.PIEC.ImobLink.Entitys.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface FollowRespository extends JpaRepository<Follow, Long> {
-    List<Follow> findByFollowerId(Long followerId);
-    List<Follow> findByFollowingId(Long followingId);
+    Optional<Follow> findByFollowerIdAndFollowingId(Long followerId, Long followingId);
 }
