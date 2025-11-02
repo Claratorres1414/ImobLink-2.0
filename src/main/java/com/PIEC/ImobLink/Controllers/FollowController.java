546e74f3c9cd0e774fc73a77ccdc41b7b2b1bed9
claratorres1414 <claratorres12345679@gmail.com>
2025-11-02 09:03:55 -0300

    #70 - Criados dois novos endpoints, o /getFollowers e o /getFollowings que fazem uso dos dois novos métodos do FollowService e servem as listas de informação recebidas.

--- src/main/java/com/PIEC/ImobLink/Controllers/FollowController.java
+++ src/main/java/com/PIEC/ImobLink/Controllers/FollowController.java
@@ -1,11 +1,14 @@
 package com.PIEC.ImobLink.Controllers;
 
+import com.PIEC.ImobLink.DTOs.UserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import com.PIEC.ImobLink.Services.FollowService;
 import com.PIEC.ImobLink.Entitys.Follow;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/follow")
@@ -26,5 +29,15 @@ public class FollowController {
     public ResponseEntity<String> unfollowUser(@PathVariable Long followingId, Authentication auth) {
         followService.unfollow(auth, followingId);
         return ResponseEntity.ok("Unfollowing " + followingId);
+    }
+
+    @GetMapping("/getFollowers")
+    public ResponseEntity<List<UserDetails>> getFollowers(Authentication auth) {
+        return ResponseEntity.ok(followService.getFollowers(auth));
+    }
+
+    @GetMapping("/getFollowings")
+    public ResponseEntity<List<UserDetails>> getFollowings(Authentication auth) {
+        return ResponseEntity.ok(followService.getFollowings(auth));
     }
 }
