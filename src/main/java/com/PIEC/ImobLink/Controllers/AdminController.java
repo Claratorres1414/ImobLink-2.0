e98a8f6866798f45819b72cb1aaa8b3dd7283303
claratorres1414 <claratorres12345679@gmail.com>
2025-11-08 09:46:32 -0300

    #87 - Atualização no AdminController para que faça uso dos novos métodos do PostService e UserService para finalidades administrativas da plataforma contando agora com endpoint para puxar quantos posts o usuário já favoritou, quantas vezes seus posts foram favoritados e quantas vezes cada post já foi favoritado

--- src/main/java/com/PIEC/ImobLink/Controllers/AdminController.java
+++ src/main/java/com/PIEC/ImobLink/Controllers/AdminController.java
@@ -1,19 +1,19 @@
 package com.PIEC.ImobLink.Controllers;
 
 import com.PIEC.ImobLink.DTOs.PromoteRequest;
+import com.PIEC.ImobLink.Services.PostService;
 import com.PIEC.ImobLink.Services.UserService;
+import jakarta.persistence.Index;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminController {
     private final UserService userService;
+    private final PostService postService;
 
     @PostMapping("/promote")
     public ResponseEntity<?> promoteUser(@RequestBody PromoteRequest request) {
@@ -23,5 +23,23 @@ public class AdminController {
             System.out.println(e.getMessage());
         }
         return ResponseEntity.ok("Usuário promovido a ADMIN com sucecsso!");
+    }
+
+    @GetMapping("/info/number/favedPosts/{userId}")
+    public ResponseEntity<Integer> getFavedPostsByUser(@PathVariable Long userId) {
+        Integer faveds = userService.calcNumberOfFavedsByUserId(userId);
+        return ResponseEntity.ok(faveds);
+    }
+
+    @GetMapping("/info/number/allPosts/favedTimes/{userId}")
+    public ResponseEntity<Integer> getFavedTimesByUser(@PathVariable Long userId) {
+        Integer favedTimes = userService.calcAllPostsFavedTimesByUserId(userId);
+        return ResponseEntity.ok(favedTimes);
+    }
+
+    @GetMapping("/info/number/favedTimes/{postId}")
+    public ResponseEntity<Integer> getFavedTimesByPost(@PathVariable Long postId) {
+        Integer favedTimes = postService.getFavedTimesByPostId(postId);
+        return ResponseEntity.ok(favedTimes);
     }
 }
