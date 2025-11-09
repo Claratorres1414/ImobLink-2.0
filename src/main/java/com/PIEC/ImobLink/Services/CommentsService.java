310f5812e834057c9ab05745a556a4e486bef9bc
claratorres1414 <claratorres12345679@gmail.com>
2025-11-09 07:00:14 -0300

    #88 - Criado o novo método commentPost que recebendo o CommentRequest, id do post e autenticação do usuário cria um novo comentário endereçado ao post e o salva na db; Criado novo método getCommentsByPostId, método responsável por listar comentários com base no id do post solicitado.

--- src/main/java/com/PIEC/ImobLink/Services/CommentsService.java
+++ src/main/java/com/PIEC/ImobLink/Services/CommentsService.java
@@ -5,6 +5,7 @@ import com.PIEC.ImobLink.DTOs.CommentResponse;
 import com.PIEC.ImobLink.Entitys.Comment;
 import com.PIEC.ImobLink.Entitys.User;
 import com.PIEC.ImobLink.Repositorys.CommentRepository;
+import com.PIEC.ImobLink.Repositorys.PostRepository;
 import com.PIEC.ImobLink.Repositorys.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -18,6 +19,7 @@ import java.util.List;
 public class CommentsService {
     private final CommentRepository commentRepository;
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
     public CommentResponse comment(CommentRequest req, Long userId, Authentication auth){
         String email = auth.getName();
@@ -40,12 +42,45 @@ public class CommentsService {
         }
     }
 
+    public CommentResponse commentPost(CommentRequest req, Long postId, Authentication auth){
+        String email = auth.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        LocalDateTime createdAt = LocalDateTime.now();
+
+        Comment comment = new Comment();
+        comment.setContent(req.getContent());
+        comment.setCreatedAt(createdAt);
+        comment.setUser(postRepository.findById(postId).get().getUser());
+        comment.setAuthor(user);
+        comment.setPost(postRepository.findById(postId).get());
+
+        try{
+            commentRepository.save(comment);
+            return new CommentResponse(comment);
+        }catch (Exception e){
+            throw new RuntimeException("Comment creation failed");
+        }
+    }
+
     public List<CommentResponse> getCommentsByUserId(Long userId, Authentication auth){
         String email = auth.getName();
         userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         return commentRepository.findAllByUserIdOrderByCreatedAtDesc(userId)
+                .stream()
+                .map(CommentResponse :: new)
+                .toList();
+    }
+
+    public List<CommentResponse> getCommentsByPostId(Long postId, Authentication auth){
+        String email = auth.getName();
+        userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return commentRepository.findAllByPostIdOrderByCreatedAtDesc(postId)
                 .stream()
                 .map(CommentResponse :: new)
                 .toList();
