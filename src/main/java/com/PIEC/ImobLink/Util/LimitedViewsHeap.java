553583a36703dd8e1a5045bb6ceff2138e779bf7
claratorres1414 <claratorres12345679@gmail.com>
2025-11-11 17:16:47 -0300

    #94 - Criada a nova classe Util o LimitedViewsHeap que faz um heap de PostResponses's baseado na quantidade de views do mesmo, tendo um tamanho máximo de 10 posts, ou seja, o heap suporta no máximo 10 publicações, se passar disso ele adiciona o novo post, verifica qual o que tem menos views e o remove

---
+++ src/main/java/com/PIEC/ImobLink/Util/LimitedViewsHeap.java
@@ -0,0 +1,29 @@
+package com.PIEC.ImobLink.Util;
+
+import com.PIEC.ImobLink.DTOs.PostResponse;
+import lombok.Getter;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+@Getter
+public class LimitedViewsHeap {
+    private PriorityQueue<PostResponse> heap;
+    private final int maxSize;
+
+    public LimitedViewsHeap() {
+        this.heap = new PriorityQueue<>(Comparator.comparingInt(PostResponse::getViews).reversed());
+        this.maxSize = 10;
+    }
+
+    public void add(PostResponse post) {
+        heap.offer(post);
+        if (heap.size() > maxSize) {
+            heap.poll();
+        }
+    }
+
+    public PriorityQueue<PostResponse> getHeap() {
+        return new PriorityQueue<>(heap);
+    }
+}
