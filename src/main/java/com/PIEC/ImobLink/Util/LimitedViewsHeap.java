206060abdd8214aba209ff56504300e99fa814a6
claratorres1414 <claratorres12345679@gmail.com>
2025-11-15 14:35:32 -0300

    #101 - Atualização no LimitedViewsHeap para que contenha um map para que eu saiba se um post já foi posto lá ou não antes de atribuir a nova quantidade de views dele no heap para que não ocorra um bug de o mesmo post ocupar as 10 posições com classificações de view diferentes agora que o heap é persistente, acrescido o método de remove que verifica se o post está dentro do map, se estiver ele é removido de lá e do heap, isso serve para o momento que o post é deleteado

--- src/main/java/com/PIEC/ImobLink/Util/LimitedViewsHeap.java
+++ src/main/java/com/PIEC/ImobLink/Util/LimitedViewsHeap.java
@@ -2,28 +2,46 @@ package com.PIEC.ImobLink.Util;
 
 import com.PIEC.ImobLink.DTOs.PostResponse;
 import lombok.Getter;
+import org.springframework.stereotype.Component;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
-@Getter
+@Component
 public class LimitedViewsHeap {
-    private PriorityQueue<PostResponse> heap;
+    @Getter
+    private final PriorityQueue<PostResponse> heap;
     private final int maxSize;
+    private final Map<Long, PostResponse> map = new HashMap<>();
+
 
     public LimitedViewsHeap() {
         this.heap = new PriorityQueue<>(Comparator.comparingInt(PostResponse::getViews));
         this.maxSize = 10;
     }
 
-    public void add(PostResponse post) {
+    public synchronized void add(PostResponse post) {
+        Long id = post.getId();
+
+        if (map.containsKey(id)) {
+            heap.remove(map.get(id));
+        }
+
+        map.put(id, post);
+
         heap.offer(post);
         if (heap.size() > maxSize) {
-            heap.poll();
+            PostResponse removed = heap.poll();
+            map.remove(removed.getId());
         }
     }
 
-    public PriorityQueue<PostResponse> getHeap() {
-        return new PriorityQueue<>(heap);
+    public synchronized void remove(long postId) {
+        if (map.containsKey(postId)) {
+            heap.remove(map.get(postId));
+            map.remove(postId);
+        }
     }
 }
