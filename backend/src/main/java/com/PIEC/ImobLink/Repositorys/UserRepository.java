dc7140f27c8cd22a80fb258d0b2f1357b6d824fc
claratorres1414 <claratorres12345679@gmail.com>
2025-11-22 10:35:22 -0300

    #110 - Implementado novo método de busca no repositório para que faça a consulta dos 10 primeiros resultados contendo a string passada na busca de User por email

--- backend/src/main/java/com/PIEC/ImobLink/Repositorys/UserRepository.java
+++ backend/src/main/java/com/PIEC/ImobLink/Repositorys/UserRepository.java
@@ -2,8 +2,11 @@ package com.PIEC.ImobLink.Repositorys;
 
 import com.PIEC.ImobLink.Entitys.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    List<User> findTop10ByEmailContainingIgnoreCase(String search);
 }
