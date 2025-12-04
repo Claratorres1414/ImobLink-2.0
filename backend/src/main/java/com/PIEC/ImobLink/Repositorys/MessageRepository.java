b3e74a9326a31499644ac15c81ef9b0b9c177c42
claratorres1414 <claratorres12345679@gmail.com>
2025-12-04 13:58:04 -0300

    #127 - Criada uma assinatura de método no MessageRepository para carregar as mensagens entre dois usuários distintos

--- backend/src/main/java/com/PIEC/ImobLink/Repositorys/MessageRepository.java
+++ backend/src/main/java/com/PIEC/ImobLink/Repositorys/MessageRepository.java
@@ -3,5 +3,8 @@ package com.PIEC.ImobLink.Repositorys;
 import com.PIEC.ImobLink.Entitys.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> searchMessageBySenderAndReceiverId(Long senderId, Long receiverId);
 }
