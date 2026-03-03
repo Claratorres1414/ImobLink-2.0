96df2e435f3e21c82ce4e70f0a93a28b3fa17249
Claratorres1414 <claratorres12345679@gmail.com>
2026-03-03 11:15:45 -0300

    Ref. #182 | feat: adiciona a classe LocalFileStorageService

---
+++ backend/src/main/java/com/PIEC/ImobLink/Services/LocalFileStorageService.java
@@ -0,0 +1,41 @@
+package com.PIEC.ImobLink.Services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+public class LocalFileStorageService implements FileStorageService {
+    @Override
+    public String saveImage(MultipartFile file, Long userId) throws IOException {
+        String folder = "uploads/users/" + userId;
+        Files.createDirectories(Paths.get(folder));
+
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();;
+        String filePath = folder + "/"+ fileName;
+
+        Files.write(Paths.get(filePath), file.getBytes());
+
+        return filePath;
+    }
+
+    @Override
+    public String saveUserProfileImage (MultipartFile file, Long userId) throws IOException {
+        String folder = "uploads/users/" + userId + "/profile";
+        Files.createDirectories(Paths.get(folder));
+
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        String filePath = folder + "/"+ fileName;
+
+        Files.write(Paths.get(filePath), file.getBytes());
+
+        return filePath;
+    }
+
+    @Override
+    public byte[] readFile(String filePath) throws IOException {
+        return Files.readAllBytes(Paths.get(filePath));
+    }
+}
