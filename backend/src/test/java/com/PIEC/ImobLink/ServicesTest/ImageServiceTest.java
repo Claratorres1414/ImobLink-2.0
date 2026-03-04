1e3140cb3a323469bd2c8e10c02eaae0dd8bde75
Claratorres1414 <162813630+Claratorres1414@users.noreply.github.com>
2026-03-04 11:21:35 -0300

    Ref. #176 | test: implementa testes unitários - ImageService (#186)
    - adiciona teste unitários para todos os métodos
    - adiciona testes unitários para cobertura de erros

---
+++ backend/src/test/java/com/PIEC/ImobLink/ServicesTest/ImageServiceTest.java
@@ -0,0 +1,317 @@
+package com.PIEC.ImobLink.ServicesTest;
+
+import Role.Role;
+import com.PIEC.ImobLink.DTOs.ImageResponse;
+import com.PIEC.ImobLink.Entitys.Images;
+import com.PIEC.ImobLink.Entitys.Post;
+import com.PIEC.ImobLink.Entitys.User;
+import com.PIEC.ImobLink.Repositorys.ImageRepository;
+import com.PIEC.ImobLink.Repositorys.PostRepository;
+import com.PIEC.ImobLink.Repositorys.UserRepository;
+import com.PIEC.ImobLink.Services.FileStorageService;
+import com.PIEC.ImobLink.Services.ImageService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ImageServiceTest {
+    @Mock private ImageRepository imageRepository;
+    @Mock private UserRepository userRepository;
+    @Mock private PostRepository postRepository;
+    @Mock private FileStorageService fileStorageService;
+    @Mock private MultipartFile file;
+
+    @InjectMocks private ImageService imageService;
+
+    User user;
+    Authentication auth;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setId(70L);
+        user.setCpf("123456789");
+        user.setPhoneNumber("546244526");
+        user.setEmail("email092@email.com");
+        user.setPassword("password");
+        user.setName("name");
+        user.setRole(Role.USER);
+
+        auth = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+    }
+
+    @Test
+    void shouldSaveImageSuccessfully() throws IOException {
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+        when(file.getContentType())
+                .thenReturn("image/jpeg");
+        when(fileStorageService.saveImage(any(), anyLong()))
+                .thenReturn("fake/path/fake.jpeg");
+        when(imageRepository.save(any(Images.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        Images savedImage = imageService.saveImage(file, auth);
+
+        assertNotNull(savedImage);
+        assertEquals("image/jpeg", savedImage.getContentType());
+        assertEquals(user, savedImage.getUser());
+        assertTrue(savedImage.getFilename().contains("fake.jpeg"));
+
+        verify(imageRepository, times(1)).save(any(Images.class));
+    }
+
+    @Test
+    void shouldThrowWhenUserNotFoundOnSaveImage() {
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> imageService.saveImage(file, auth));
+    }
+
+    @Test
+    void shouldPropagateIOExceptionOnSaveImage() throws IOException {
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(fileStorageService.saveImage(any(), anyLong()))
+                .thenThrow(new IOException("Disk error"));
+
+        assertThrows(IOException.class,
+                () -> imageService.saveImage(file, auth));
+    }
+
+    @Test
+    void shouldSaveUserProfileImageSuccessfully() throws IOException {
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+        when(fileStorageService.saveUserProfileImage(any(), anyLong()))
+                .thenReturn("fake/path/fake.jpeg");
+        when(file.getContentType())
+                .thenReturn("image/jpeg");
+        when(imageRepository.save(any(Images.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        Images savedImage = imageService.saveProfileImage(file, auth);
+
+        assertNotNull(savedImage);
+        assertEquals("image/jpeg", savedImage.getContentType());
+        assertEquals(user, savedImage.getUser());
+        assertTrue(savedImage.getFilename().contains("fake.jpeg"));
+
+        verify(imageRepository, times(1)).save(any(Images.class));
+    }
+
+    @Test
+    void shouldUpdateUserProfileImageId() throws IOException {
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(fileStorageService.saveUserProfileImage(any(), anyLong()))
+                .thenReturn("fake/path/fake.jpeg");
+
+        when(file.getContentType())
+                .thenReturn("image/jpeg");
+
+        when(imageRepository.save(any(Images.class)))
+                .thenAnswer(invocation -> {
+                    Images img = invocation.getArgument(0);
+                    img.setId(99L);
+                    return img;
+                });
+
+        imageService.saveProfileImage(file, auth);
+
+        assertEquals(99L, user.getImageProfileId());
+
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    void shouldThrowWhenUserNotFoundOnSaveProfileImage() {
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> imageService.saveProfileImage(file, auth));
+    }
+
+    @Test
+    void shouldReturnFirstImageBytesByPostId() throws IOException {
+
+        // Arrange
+        Images image = new Images();
+        image.setFilepath("fake/path/image.jpeg");
+
+        Post post = new Post();
+        post.setId(1L);
+        post.setImages(List.of(image));
+
+        byte[] fakeBytes = "fake-image-content".getBytes();
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(postRepository.getPostById(anyLong()))
+                .thenReturn(post);
+
+        when(fileStorageService.readFile("fake/path/image.jpeg"))
+                .thenReturn(fakeBytes);
+
+        byte[] result = imageService.getFirstImageByPostId(1L, auth);
+
+        assertNotNull(result);
+        assertArrayEquals(fakeBytes, result);
+
+        verify(fileStorageService).readFile("fake/path/image.jpeg");
+    }
+
+    @Test
+    void shouldThrowWhenPostHasNoImages() {
+
+        Post post = new Post();
+        post.setImages(List.of());
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(postRepository.getPostById(anyLong()))
+                .thenReturn(post);
+
+        assertThrows(NoSuchElementException.class,
+                () -> imageService.getFirstImageByPostId(1L, auth));
+    }
+
+    @Test
+    void shouldReturnAllImagesByPostId() {
+        Images img1 = new Images();
+        img1.setId(1L);
+        img1.setFilename("img1.jpeg");
+        img1.setFilepath("path/1");
+        img1.setContentType("image/jpeg");
+
+        Images img2 = new Images();
+        img2.setId(2L);
+        img2.setFilename("img2.jpeg");
+        img2.setFilepath("path/2");
+        img2.setContentType("image/jpeg");
+
+        Post post = new Post();
+        post.setImages(List.of(img1, img2));
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(postRepository.findById(anyLong()))
+                .thenReturn(Optional.of(post));
+
+        List<ImageResponse> response =
+                imageService.getAllImagesByPostId(1L, auth);
+
+        assertEquals(2, response.size());
+        assertEquals("img1.jpeg", response.get(0).getFilename());
+        assertEquals("img2.jpeg", response.get(1).getFilename());
+    }
+
+    @Test
+    void shouldThrowWhenPostNotFound() {
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(postRepository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> imageService.getAllImagesByPostId(1L, auth));
+    }
+
+    @Test
+    void shouldReturnImageBytesByImageId() throws IOException {
+
+        Images image = new Images();
+        image.setId(1L);
+        image.setFilepath("fake/path/image.jpeg");
+
+        byte[] fakeBytes = "fake-image".getBytes();
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(imageRepository.findById(anyLong()))
+                .thenReturn(Optional.of(image));
+
+        when(fileStorageService.readFile("fake/path/image.jpeg"))
+                .thenReturn(fakeBytes);
+
+        byte[] result = imageService.getImageById(1L, auth);
+
+        assertArrayEquals(fakeBytes, result);
+
+        verify(fileStorageService).readFile("fake/path/image.jpeg");
+    }
+
+    @Test
+    void shouldThrowWhenImageNotFound() {
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(imageRepository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NoSuchElementException.class,
+                () -> imageService.getImageById(1L, auth));
+    }
+
+    @Test
+    void shouldThrowWhenAuthenticatedUserNotFound() {
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> imageService.getImageById(1L, auth));
+    }
+
+    @Test
+    void shouldPropagateIOExceptionOnReadFile() throws IOException {
+
+        Images image = new Images();
+        image.setFilepath("fake/path/image.jpeg");
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(imageRepository.findById(anyLong()))
+                .thenReturn(Optional.of(image));
+
+        when(fileStorageService.readFile(anyString()))
+                .thenThrow(new IOException("Read error"));
+
+        assertThrows(IOException.class,
+                () -> imageService.getImageById(1L, auth));
+    }
+}
