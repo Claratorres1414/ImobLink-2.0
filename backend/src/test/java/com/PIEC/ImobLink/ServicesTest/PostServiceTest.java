85d38717b3e8f10a45cde9b21818035367619b66
Claratorres1414 <claratorres12345679@gmail.com>
2026-03-12 12:29:56 -0300

    Ref. #178 | test(post-service): cria testes unitários para o método create-post.

--- backend/src/test/java/com/PIEC/ImobLink/ServicesTest/PostServiceTest.java
+++ backend/src/test/java/com/PIEC/ImobLink/ServicesTest/PostServiceTest.java
@@ -1,6 +1,8 @@
 package com.PIEC.ImobLink.ServicesTest;
 
 import Role.Role;
+import com.PIEC.ImobLink.DTOs.PostResponse;
+import com.PIEC.ImobLink.Entitys.Images;
 import com.PIEC.ImobLink.Entitys.Post;
 import com.PIEC.ImobLink.Entitys.User;
 import com.PIEC.ImobLink.Repositorys.*;
@@ -10,13 +12,26 @@ import com.PIEC.ImobLink.Util.FavsLimitedHeap;
 import com.PIEC.ImobLink.Util.LikesLimitedHeap;
 import com.PIEC.ImobLink.Util.ViewsLimitedHeap;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
@@ -36,6 +51,8 @@ public class PostServiceTest {
     User user;
     Authentication auth;
     Post post;
+    List<MultipartFile> images;
+    Images image;
 
     @BeforeEach
     void setUp() {
@@ -58,5 +75,56 @@ public class PostServiceTest {
         post.setPrice(5000);
         post.setUser(user);
         auth = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+
+        mFile = new MockMultipartFile("image", "img.jpg", "image/jpeg", "data".getBytes());
+
+        image = new Images();
+        image.setId(10L);
+        image.setFilename("img.jpg");
+
+        images = new ArrayList<>();
+        images.add(mFile);
+    }
+
+    @Test
+    void shouldCreateANewPost() throws IOException {
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        when(imageService.saveImage(any(), any()))
+                .thenReturn(image);
+
+        when(postRepository.save(any(Post.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        PostResponse response = postService.createPost(images, post.getDescription(), post.getPrice(), post.getStreet(), post.getAvenue(), post.getNumber(), post.getType(), auth);
+
+        assertNotNull(response);
+        assertEquals(response.getUserId(), user.getId());
+
+        verify(userRepository, times(1)).findByEmail(anyString());
+        verify(postRepository, times(1)).save(any(Post.class));
+    }
+
+    @Test
+    void shouldNotCreateANewPostWhenUserNotFound() {
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.empty());
+
+        Authentication authFake = new UsernamePasswordAuthenticationToken(user.getEmail(), "a");
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> postService.createPost(images, post.getDescription(), post.getPrice(), post.getStreet(), post.getAvenue(), post.getNumber(), post.getType(), authFake));
+    }
+
+    @Test
+    void shouldNotCreateANewPostWhenPostHasNoImages() {
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(user));
+
+        List<MultipartFile> imagesFake = new ArrayList<>();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> postService.createPost(imagesFake, post.getDescription(), post.getPrice(), post.getStreet(), post.getAvenue(), post.getNumber(), post.getType(), auth));
     }
 }
