a428f0f6e3e7583dcce33513663837d05a12e4fb
Claratorres1414 <claratorres12345679@gmail.com>
2026-04-23 17:30:39 -0300

    feat: integra funcionalidade de login ao servidor (Ref. #218)

--- mobile/src/navigation/index.tsx
+++ mobile/src/navigation/index.tsx
@@ -2,9 +2,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
 import AppStack from './AppStack';
 import {useAuthStore} from "../store/authStore";
+import {ActivityIndicator, View} from "react-native";
 
 export default function Routes() {
-    const token = useAuthStore((state) => state.token);
+    const { token, loading } = useAuthStore();
+
+    if (loading) {
+        return (
+            <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
+                <ActivityIndicator />
+            </View>
+        )
+    }
 
     return (
         <NavigationContainer>
