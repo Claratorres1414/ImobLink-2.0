e056fc6d3b3f9048c41d33608e5fbd8bcfdfcf78
Claratorres1414 <claratorres12345679@gmail.com>
2026-06-02 11:25:53 -0300

    feat: implementa cloudinary-service para armazenamento em prod (Ref. #271)

--- backend/src/main/java/com/PIEC/ImobLink/Services/Images/CloudinaryStorageService.java
+++ backend/src/main/java/com/PIEC/ImobLink/Services/Images/CloudinaryStorageService.java
@@ -1,4 +1,61 @@
 package com.PIEC.ImobLink.Services.Images;
 
-public class CloudinaryStorageService {
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Map;
+
+@Service
+@Profile("prod")
+@RequiredArgsConstructor
+public class CloudinaryStorageService implements FileStorageService {
+    private final Cloudinary cloudinary;
+
+    @Override
+    public String saveImage(MultipartFile file, Long userId) {
+        try {
+            Map<?, ?> result = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.asMap(
+                            "folder", "posts/" + userId
+                    )
+            );
+
+            return result.get("secure_url").toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao enviar imagem para o Cloudinary", e);
+        }
+    }
+
+    @Override
+    public String saveUserProfileImage(MultipartFile file, Long userId) {
+        try {
+            Map<?, ?> result = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.asMap(
+                            "folder", "profiles/" + userId
+                    )
+            );
+
+            return result.get("secure_url").toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao enviar imagem de perfil para o Cloudinary", e);
+        }
+    }
+
+    @Override
+    public byte[] readFile(String filePath) {
+        try (InputStream inputStream = new URL(filePath).openStream()) {
+            return inputStream.readAllBytes();
+        }  catch (IOException e) {
+            throw new RuntimeException("Erro ao ler imagem do Cloudinary", e);
+        }
+    }
 }
