8e9f027ecd842c9243e6dee19bb0187342f03ece
Claratorres1414 <claratorres12345679@gmail.com>
2026-06-02 13:21:33 -0300

    feat: passa a rodar scripts de inicialização apenas em dev (Ref. #272)

--- backend/src/main/java/com/PIEC/ImobLink/Initializer/UsersInitializer.java
+++ backend/src/main/java/com/PIEC/ImobLink/Initializer/UsersInitializer.java
@@ -5,11 +5,13 @@ import com.PIEC.ImobLink.Entitys.User;
 import com.PIEC.ImobLink.Repositorys.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Profile("dev")
 public class UsersInitializer implements CommandLineRunner {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
