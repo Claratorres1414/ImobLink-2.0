b99f05d4b1598f1bd732386468746f86f8d8533a
Clara Torres <162813630+Claratorres1414@users.noreply.github.com>
2026-06-04 17:05:53 -0300

    feat: hospedagem da data base (Ref. #275) (#276)
    * feat: ajusta env initializer e traz tolerância de tempo para prod.properties (Ref. #275)
    
    * feat: ajusta properties para usar perfil de APP_DB_USER após configurações finais da db (Ref. #275)

--- backend/src/main/java/com/PIEC/ImobLink/Initializer/EnvInitializer.java
+++ backend/src/main/java/com/PIEC/ImobLink/Initializer/EnvInitializer.java
@@ -3,6 +3,10 @@ package com.PIEC.ImobLink.Initializer;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EnvInitializer
         implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -10,16 +14,21 @@ public class EnvInitializer
     @Override
     public void initialize(ConfigurableApplicationContext context) {
 
-        String profile =
-                System.getProperty("spring.profiles.active", "dev");
+        String profile = context.getEnvironment()
+                .getProperty("spring.profiles.active", "dev");
+
+        System.out.println("PROFILE = " + profile);
 
         Dotenv dotenv = Dotenv.configure()
                 .filename(".env." + profile)
                 .ignoreIfMissing()
                 .load();
 
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
+        Map<String, Object> envMap = new HashMap<>();
+        dotenv.entries().forEach(entry -> envMap.put(entry.getKey(), entry.getValue()));
+
+        context.getEnvironment()
+                .getPropertySources()
+                .addFirst(new MapPropertySource("customEnvProperties", envMap));
     }
 }
