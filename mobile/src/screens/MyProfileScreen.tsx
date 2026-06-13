66c961a72a575cb129d22217566a6f1db95f1222
Claratorres1414 <claratorres12345679@gmail.com>
2026-06-13 08:59:31 -0300

    feat: transforma header de user info em componente (Ref. #283)

--- mobile/src/screens/MyProfileScreen.tsx
+++ mobile/src/screens/MyProfileScreen.tsx
@@ -3,14 +3,15 @@ import {RootStackParamList} from "../navigation/types";
 
 import Tabbar from "../components/Tabbar";
 import {useFocusEffect, useNavigation} from "@react-navigation/native";
-import {Image, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import React, {useCallback, useEffect, useState} from "react";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import React, {useCallback, useState} from "react";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
-import { Phone, Mail } from 'lucide-react-native';
 import {getUserInfo} from "../apiServices/userService";
 import {getMyFavs, getMyPosts} from "../apiServices/postService";
 import {mapPostFromApi} from "../mappers/postMapper";
 import MiniPostCardSlider from "../components/MiniPostCardSlider";
+import UserInfoPageHeader from "../components/UserInfoPageHeader";
+import {getProfileImage} from "../apiServices/imageService";
 
 type MyProfileNavigationProp =
     NativeStackNavigationProp<
@@ -33,6 +34,12 @@ export default function MyProfileScreen() {
             const data = await getUserInfo();
 
             setUser(data.data);
+
+            const imageUri = await getProfileImage(
+                data.data.imageProfileId
+            );
+
+            setProfileImage(imageUri);
         } catch (error) {
             console.log(
                 "Erro ao carregar usuário:",
@@ -88,62 +95,7 @@ export default function MyProfileScreen() {
                         contentContainerStyle={{ paddingBottom: 20 }}
 
                     >
-                        <View style={styles.userInfo}>
-                            <Image
-                                source={
-                                    profileImage
-                                        ? { uri: profileImage }
-                                        : require("../assets/default_profile.jpg")
-                                }
-                                style={styles.avatar}
-                            />
-
-                            <View style={styles.infoColumn}>
-                                <Text style={styles.username}>
-                                    {user?.name || "User"}
-                                </Text>
-
-                                <View style={styles.infoBox}>
-                                    <TouchableOpacity style={styles.infoItem}>
-                                        <Text style={styles.infoNumbers}>
-                                            {myPosts?.length || 0}
-                                        </Text>
-                                        <Text style={styles.infoText}>posts</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={styles.infoItem}>
-                                        <Text style={styles.infoNumbers}>
-                                            {user?.followers || 0}
-                                        </Text>
-                                        <Text style={styles.infoText}>seguidores</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={styles.infoItem}>
-                                        <Text style={styles.infoNumbers}>
-                                            {user?.followings || 0}
-                                        </Text>
-                                        <Text style={styles.infoText}>seguindo</Text>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                        </View>
-                        <View style={styles.detailsBox}>
-                            <View style={styles.detailRow}>
-                                <Phone size={16} color="#7D92D4" fill="#7D92D4"/>
-                                <Text style={styles.detailText}>
-                                    {user?.phoneNumber || "+55 (81) 99999-9999"}
-                                </Text>
-                            </View>
-
-                            <View style={styles.detailRow}>
-                                <Mail size={16} color="#7D92D4"/>
-                                <Text style={styles.detailText}>
-                                    {user?.email || "email@email.com"}
-                                </Text>
-                            </View>
-
-                            <Text style={styles.bioText}>
-                                {user?.bio || "Corretor de imóveis trabalhando no ramo a mais de 25 anos"}
-                            </Text>
-                        </View>
+                        <UserInfoPageHeader user={user} profileImage={profileImage} postsNum={myPosts.length}/>
                         <TouchableOpacity style={styles.editProfileButton}>
                             <Text style={styles.editProfileText}>
                                 Editar perfil
@@ -194,76 +146,7 @@ export default function MyProfileScreen() {
 }
 
 const styles = StyleSheet.create({
-    userInfo: {
-        flexDirection: "row",
-        alignItems: "flex-start",
-        padding: 23,
-    },
-    avatar: {
-        height: 100,
-        width: 100,
-        borderRadius: 70,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 4,
-    },
-    infoColumn: {
-        flex: 1,
-        marginLeft: 24,
-        alignItems: "flex-start",
-    },
-    username: {
-        fontFamily: "Inter-Bold",
-        fontSize: 20,
-        fontWeight: "700",
-        color: "#7D92D4",
-        marginBottom: 5,
-    },
-    infoBox: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 30,
-    },
-    infoItem: {
-        alignItems: "center",
-    },
-    infoNumbers: {
-        color: "#7D92D4",
-        fontFamily: "Inter-Bold",
-        fontSize: 25,
-        fontWeight: "700",
-    },
-    infoText: {
-        color: "#7D92D4",
-        fontFamily: "Inter-SemiBold",
-        fontSize: 12,
-        fontWeight: "600",
-    },
-    detailsBox: {
-        paddingHorizontal: 27,
-        paddingBottom: 12,
-    },
-    detailRow: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 8,
-        marginBottom: 8,
-    },
-    detailText: {
-        fontFamily: "Inter-SemiBold",
-        fontSize: 15,
-        fontWeight: 200,
-        color: "#7D92D4",
-    },
-    bioText: {
-        fontFamily: "Inter-SemiBold",
-        fontSize: 15,
-        fontWeight: 200,
-        color: "#7D92D4",
-        marginTop: 4,
-    },
+
     editProfileButton: {
         width: "auto",
         height: 28,
