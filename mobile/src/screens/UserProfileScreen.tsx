71a4de7d5d8da0549e42c4c303289b3732c603b1
Claratorres1414 <claratorres12345679@gmail.com>
2026-06-16 09:26:47 -0300

    feat: implementa componente na tela de perfil do usuário (Ref. #283)

--- mobile/src/screens/UserProfileScreen.tsx
+++ mobile/src/screens/UserProfileScreen.tsx
@@ -1,9 +1,9 @@
-import {RouteProp, useNavigation} from "@react-navigation/native";
+import {RouteProp, useFocusEffect, useNavigation} from "@react-navigation/native";
 import {RootStackParamList} from "../navigation/types";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {ArrowLeft} from "lucide-react-native";
-import React from "react";
+import React, {useCallback, useState} from "react";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import UserInfoPageHeader from "../components/UserInfoPageHeader";
 import {useAuthStore} from "../store/authStore";
@@ -11,6 +11,9 @@ import EditProfileButton from "../components/EditProfileButton";
 import FollowButton from "../components/FollowButton";
 import ChatButton from "../components/ChatButton";
 import FollowAndChatBar from "../components/FollowAndChatBar";
+import MyProfileSlidersGroup from "../components/MyProfileSlidersGroup";
+import {getMyFavs, getMyPosts} from "../apiServices/postService";
+import {mapPostFromApi} from "../mappers/postMapper";
 
 type Props = {
     route: RouteProp<
@@ -31,12 +34,56 @@ export default function UserProfileScreen({route}: Props) {
 
     const insets = useSafeAreaInsets();
 
+    const [userPosts, setUserPosts] = useState<any[]>([]);
+    const [userFavs, setUserFavs] = useState<any[]>([]);
+
     const {user, imageProfile} = route.params;
     const currentUserId =
         useAuthStore((state) => state.userId);
 
+    if (user.id === currentUserId) {
+        async function loadMyPosts() {
+            try {
+                const data = await getMyPosts();
+                const mapped = Array.isArray(data.data)
+                    ? data.data.map(mapPostFromApi)
+                    : [];
+
+                setUserPosts(mapped)
+            } catch (error) {
+                console.log(
+                    "Erro ao carregar posts do usuário:",
+                    error
+                )
+            }
+        }
+
+        async function loadMyFavs() {
+            try {
+                const data = await getMyFavs();
+                const mapped = Array.isArray(data.data)
+                    ? data.data.map(mapPostFromApi)
+                    : [];
+
+                setUserFavs(mapped)
+            } catch (error) {
+                console.log(
+                    "Erro ao carregar posts favoritos do usuário:",
+                    error
+                )
+            }
+        }
+
+        useFocusEffect(
+            useCallback(() => {
+                loadMyPosts();
+                loadMyFavs();
+            }, [])
+        );
+    }
+
     return (
-        <View style={{ flex:1, paddingTop: insets.top }}>
+        <ScrollView style={{ flex:1, paddingTop: insets.top }}>
             <TouchableOpacity
                 style={styles.backButton}
                 activeOpacity={0.7}
@@ -52,14 +99,17 @@ export default function UserProfileScreen({route}: Props) {
             {
                 user &&
                 user.id === currentUserId && (
-                    <EditProfileButton/>
+                    <View>
+                        <EditProfileButton/>
+                        <MyProfileSlidersGroup myFavs={userFavs} myPosts={userPosts} />
+                    </View>
                 ) || (
                     <View style={{alignItems: "center"}}>
                         <FollowAndChatBar userId={user.id}/>
                     </View>
                 )
             }
-        </View>
+        </ScrollView>
     );
 }
 
