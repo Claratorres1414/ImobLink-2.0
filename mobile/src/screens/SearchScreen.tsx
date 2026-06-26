812acb9ac165a7e8357c9405e987868156c5733b
Claratorres1414 <claratorres12345679@gmail.com>
2026-06-26 15:35:34 -0300

    feat: implementa componente de barra de pesquisa (Ref. #285)

--- mobile/src/screens/SearchScreen.tsx
+++ mobile/src/screens/SearchScreen.tsx
@@ -1,10 +1,11 @@
 import {RouteProp, useNavigation} from "@react-navigation/native";
 import {RootStackParamList} from "../navigation/types";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import {SafeAreaView, useSafeAreaInsets} from "react-native-safe-area-context";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 import Tabbar from "../components/Tabbar";
-import React from "react";
+import React, {useState} from "react";
 import {View} from "react-native";
+import {SearchBar} from "../components/SearchBar";
 
 type Props = {
     route: RouteProp<
@@ -23,9 +24,16 @@ export default function SearchScreen({route}: Props) {
     const navigation = useNavigation<NavigationProps>();
     const insets = useSafeAreaInsets();
 
+    const [search, setSearch] = useState('');
+
     return (
-        <View style={{ flex:1, paddingTop: insets.top }}>
-            <View style={{ flex: 1 }}></View>
+        <View style={{ flex:1, paddingTop: insets.top, alignItems: "center" }}>
+            <View style={{ flex: 1 }}>
+                <SearchBar
+                    value={search}
+                    onChangeText={setSearch}
+                />
+            </View>
             <Tabbar
                 activeTab={"search"}
                 onAddPress={() => navigation.navigate("CreateNewPost")}
