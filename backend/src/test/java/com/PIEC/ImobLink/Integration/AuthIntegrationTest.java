84c98eee411567dc87098e001c927f1164623e64
Claratorres1414 <claratorres12345679@gmail.com>
2026-08-12 14:16:09 -0300

    feat: implementa teste para cadastro de usuário (Ref. #301)

--- backend/src/test/java/com/PIEC/ImobLink/Integration/AuthIntegrationTest.java
+++ backend/src/test/java/com/PIEC/ImobLink/Integration/AuthIntegrationTest.java
@@ -1,8 +1,35 @@
 package com.PIEC.ImobLink.Integration;
 
+import com.PIEC.ImobLink.DTOs.RegisterRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 
-public class AuthIntegrationTest extends IntegrationTest{
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthIntegrationTest extends IntegrationTest {
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    void shouldRegisterUserSuccessfully() {}
+    void shouldRegisterUserSuccessfully() throws Exception {
+        RegisterRequest request = new RegisterRequest();
+
+        request.setCpf("000.000.000-00");
+        request.setPhoneNumber("(00) 90000-0000");
+        request.setName("Fulano de tal");
+        request.setEmail("fulano@email.com");
+        request.setPassword("123456789101112");
+
+        mockMvc.perform(
+                post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        )
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
 }
