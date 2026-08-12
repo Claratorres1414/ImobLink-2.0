4a849bbf160b8d6f9ba72ec3166f2d6593dff236
Claratorres1414 <claratorres12345679@gmail.com>
2026-08-12 13:41:30 -0300

    feat: cria classe para testes de integração de autenticação (Ref. #301)

---
+++ backend/src/test/java/com/PIEC/ImobLink/Integration/AuthIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.PIEC.ImobLink.Integration;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthIntegrationTest extends IntegrationTest{
+    @Test
+    void shouldRegisterUserSuccessfully() {}
+}
